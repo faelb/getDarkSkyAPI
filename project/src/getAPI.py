086741fe34ab14a28fb4d4a5c9bde6d6624338be
faelb <faelb@gmx.at>
2020-04-16 22:27:26 +0200

    safety api weg

--- project/src/getAPI.py
+++ project/src/getAPI.py
@@ -1,4 +1,20 @@
+"""
+    File name: getAPI.py
+    Author: faelb (faelb@gmx.at)
+    Date created: 16/04/2020
+    Date last modified: 16/04/2020
+    Python Version: 3.8
+   This program fetches data from the DarkSkyAPI
+   to create dummy-data for our SMAROO db.
+"""
+
 import mysql.connector
+from darksky.api import DarkSky, DarkSkyAsync
+from darksky.types import languages, units, weather
+from datetime import datetime as dt  # for timemachine API
+
+API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+darksky = DarkSky(API_KEY)
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -6,14 +22,53 @@ mydb = mysql.connector.connect(
     passwd="faelb",
     database="smaroo_db"
 )
-
-print(mydb)
-
 myCursor = mydb.cursor()
+format = '%Y-%m-%d %H:%M:%S'
+for i in range(2, 30):
+    t = dt(2020, 2, i, 3, 0, 0)  # for some reason the API gives day -1 thats why we start at 2 to 30
 
-myCursor.execute("SELECT * FROM temperatur")
+    latitude = 48.210033
+    longitude = 16.363449
+    forecast = darksky.get_time_machine_forecast(
+        latitude, longitude,
+        extend=False,  # default `False`
+        lang=languages.ENGLISH,  # default `ENGLISH`
+        values_units=units.SI,  # default `auto`
+        exclude=[weather.MINUTELY, weather.ALERTS],  # default `[]`,
+        timezone='GMT',  # default None - will be set by DarkSky API automatically
+        time=t
+    )
 
-fetchresult = myCursor.fetchall()
+    for item in forecast.daily:
+        a = item.time
+        b = item.apparent_temperature_high
+        c = item.humidity
 
-for x in fetchresult:
-    print(x)
+    sql = "INSERT INTO temperatur (Zeitpunkt, temperatur) VALUES (%s, %s)"
+    values = (a.strftime(format), int(b))
+    myCursor.execute(sql, values)
+    sql = "INSERT INTO humidity (Zeitpunkt, humidity) VALUES (%s, %s)"
+    values = (a.strftime(format), c)
+    myCursor.execute(sql, values)
+
+    mydb.commit()
+
+print("Fertig")
+
+# print(forecast.latitude)
+# print(forecast.longitude)
+# print(forecast.timezone)
+
+# [print(i.apparent_temperature_high) for i in forecast.daily] #because daily is a list (but with only one item?)
+# print(len(forecast.daily.data)) #yes only one item in there - makes sense for a daily request
+# print(f"temp: {forecast.currently.temperature}")
+
+
+# print(mydb)
+#
+# myCursor.execute("SELECT * FROM temperatur")
+#
+# fetchresult = myCursor.fetchall()
+#
+# for x in fetchresult:
+#     print(x)
